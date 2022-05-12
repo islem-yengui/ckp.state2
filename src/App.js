96480a0d7d359157d455e79cc2ac26fc01ce0aa2
islem-yengui <islem.yengui@hotmail.com>
2022-05-12 14:46:03 +0000

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Person from './Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName : "Islem Yengui",
+      bio : "I just love chocolat",
+      imgSrc : 'photo.jpg',
+      profession : "student",
+    },
+    show : true,
+    counter:0
+    
+  };
+  componentDidMount() {
+    setInterval(()=>{
+      this.tick();
+    },1000);
+  }
+  
+
+  tick() {
+    this.setState({counter:this.state.counter +1 });
+  }
+
+  handleShowPerson = () => {
+    this.setState({
+       show: !this.state.show,
+    });
+  }
+  render() {
+    return (
+      <>
+      {
+        this.state.show &&
+        <div>
+          <h1>{this.state.Person.fullName}</h1>
+          <h1>{this.state.Person.bio}</h1>
+          <img src={this.state.Person.imgSrc} ></img>
+          <h1>{this.state.Person.profession}</h1>
+          <h1>{this.state.counter}</h1>
+        </div>
+        }
+        <br></br>
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = ({fullName,bio,profession}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Person
